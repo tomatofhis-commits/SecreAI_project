d303898f35a72f34e8092bb686d97e87aca7aa86
tomatofhis-commits <tomato.fhis@gmail.com>
2026-02-12 03:50:05 +0900

    不要な作業用ファイルを削除

--- Ollama_1060.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-taskkill /f /im ollama.exe >nul 2>&1
-timeout /t 2 /nobreak
-set CUDA_VISIBLE_DEVICES=GPU-bf332029-a056-f265-7930-3cfecf2ac71e
-start "" "%LOCALAPPDATA%\Programs\Ollama\ollama app.exe"
-exit
--- Ollama_Dual.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-taskkill /f /im ollama.exe >nul 2>&1
-timeout /t 2 /nobreak
-set CUDA_VISIBLE_DEVICES=GPU-bf332029-a056-f265-7930-3cfecf2ac71e,GPU-9fe1036e-2000-3b5f-e356-2eec3a7129f9
-set OLLAMA_SCHED_SPREAD=1
-start "" "%LOCALAPPDATA%\Programs\Ollama\ollama app.exe"
-exit
